97f7e81a0554b81ad88993fd3dc8b5be5d9325a0
ServerToMbak <server78135@gmail.com>
2022-11-22 21:08:57 +0300

    Updates

---
+++ src/app/components/category/category.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { Category } from 'src/app/Models/category';
+import { CategoryService } from 'src/app/services/category.service';
+
+@Component({
+  selector: 'app-category',
+  templateUrl: './category.component.html',
+  styleUrls: ['./category.component.css']
+})
+export class CategoryComponent implements OnInit {
+  categories:Category[] = [];
+  currentCategory:Category
+  constructor(private categoryService:CategoryService) { }
+
+  ngOnInit(): void {
+    this.getCategories();
+  }
+  getCategories(){
+
+    this.categoryService.getCategories().subscribe(response=>{
+     this.categories=response.data
+    });
+   }
+    setCurrentCategory(category:Category){
+      this.currentCategory=category;
+   }
+}
