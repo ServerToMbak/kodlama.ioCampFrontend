ed3ff0047742ffca3120b55aa2fddf248f4f4a5c
ServerToMbak <server78135@gmail.com>
2022-12-08 13:16:48 +0300

    Product-add Completed and validation detail showing as a toastrService

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ListResponseModel } from '../Models/listResponseNodel';
 import { Product } from '../Models/Product';
+import { ResponseModel } from '../Models/responseModel';
 
 
 @Injectable({
@@ -22,5 +23,10 @@ export class ProductService {
   getProductsByCategory(categoryId:number):Observable<ListResponseModel<Product>>{
     let newPath=this.apiUrl+"Products/getbycategory?CategoryId="+categoryId
       return this.httpClient.get<ListResponseModel<Product>>(newPath)
+  }
+
+
+  add(product:Product):Observable<ResponseModel>{
+    return this.httpClient.post<ResponseModel>(this.apiUrl+"products/add",product);
   }
 }
